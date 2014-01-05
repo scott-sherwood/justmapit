a3a2d24bb03001de055087f05dcb4a676412c928
Scott Sherwood <scott@scott-sherwood.com>
2014-01-05 16:23:30 +0000

    Project Start

---
+++ JustMapIt/Geometry/Point.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * JustMapIt Point
+ *
+ * @file    JustMapit/Geometry/Point.php
+ * @desc    Describes a line on the earth.
+ * @author  Scott Sherwood
+ * @license BSD/GPLv2
+ *
+ * (c) copyright Scott Sherwood (Scott Sherwood) and the JustMapIt Community.
+ * This source file is subject to the BSD/GPLv2 License that is bundled
+ * with this source code in the file license.txt.
+ */
+class Point{
+	
+
+}
